6a0bd8ecca79ac8e2dd1de463f3fe5959975bc92
Kotaro0722 <kotaro.tanaka@4nonome.com>
2024-06-04 00:26:45 +0900

    大体作成

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { DashboardLayout } from "./layout";
+
+export type TaskType = {
+  name: string;
+  detail: string;
+  link: string;
+};
+
+export const Dashboard = () => {
+  const tasks: TaskType[] = [
+    {
+      name: "自然言語処理",
+      detail: "耳コピ",
+      link: "https://sola.sus.ac.jp/mod/assign/view.php?id=92756",
+    },
+  ];
+  return <DashboardLayout tasks={tasks} />;
+};
---
+++ src/components/dashboard/layout.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { TaskType } from ".";
+import { Task } from "../../parts/dashboard";
+
+type DashboardLayoutProps = {
+  tasks: TaskType[];
+};
+
+export const DashboardLayout = ({ tasks }: DashboardLayoutProps) => {
+  return (
+    <Wrapper>
+      <Title>課題一覧</Title>
+      {tasks.map((task, index) => {
+        return (
+          <Task
+            name={task.name}
+            detail={task.detail}
+            link={task.link}
+            key={index}
+          />
+        );
+      })}
+    </Wrapper>
+  );
+};
+
+const Title = styled.header`
+  display: flex;
+  justify-content: center;
+  color: white;
+  font-size: 40px;
+`;
+
+const Wrapper = styled.div`
+  background-color: #282c34;
+  width: 100vw;
+  height: 100vh;
+`;
